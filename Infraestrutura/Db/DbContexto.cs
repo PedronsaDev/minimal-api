d00ae0a0f13aba5644cbbfab6d91d53e8b52e227
PedronsaDev <pedro.achilles@live.com>
2025-08-22 20:06:46 -0300

    Add database

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,7 +1,28 @@
-﻿namespace MinimalApi.Infraestrutura.Db;
+﻿using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.Entidades;
 
-public class DbContexto
+namespace MinimalApi.Infraestrutura.Db;
+
+public class DbContexto : DbContext
 {
+    private readonly IConfiguration _configurataoAppSettings;
 
+    public DbContexto(IConfiguration configurataoAppSettings)
+    {
+        _configurataoAppSettings = configurataoAppSettings;
+    }
+
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var stringConexao = _configurataoAppSettings.GetConnectionString("mysql")?.ToString();
+        if (!string.IsNullOrEmpty(stringConexao))
+        {
+            optionsBuilder.UseMySql(
+                stringConexao,
+                ServerVersion.AutoDetect(stringConexao)
+            );
+        }
+    }
 }
-
